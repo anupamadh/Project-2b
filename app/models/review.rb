a1f37f1a7fa317b9288bf2d5a5ec9ecbb3bbfb00
Anupama Dhir <sabhssabhs@yahoo.com>
2016-08-04 12:22:51 +0800

    added background image

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :product
+  validates :content, presence: true
+  validates :content, length: { minimum: 1, message: 'Please enter your review'}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,9 @@ attr_accessor :upd_user
 #validation
   validates :name, :email, :phone, :address, presence: true
   validates :email, format: { with: EMAIL_REGEX}
-  validates :phone, length: { maximum: 8, message: 'Too long'}
-  validates :address, length: { maximum: 50, message: 'Too long'}
-  validates :password, presence: true, length: { minimum: 5, message: 'Too short'}, unless: :upd_user
+  validates :phone, length: { maximum: 8, message: 'Too long. Maximum 8 characters'}
+  validates :address, length: { maximum: 50, message: 'Too long. Maximum 50 characters'}
+  validates :password, presence: true, length: { minimum: 5, message: 'Too short. Enter atleast 5 characters'}, unless: :upd_user
   validates :email, uniqueness: {case_sensitive: true}
   has_secure_password
 end
