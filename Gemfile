f0961343f79fd3d807fbdfc2a21faba2809d480b
Anupama Dhir <sabhssabhs@yahoo.com>
2016-08-05 05:32:34 +0800

    added error handling for reviews

--- Gemfile
+++ Gemfile
@@ -27,6 +27,7 @@ gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
 gem 'faker'
 gem "rails-erd"
+gem 'dynamic_form'
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :product
   validates :content, presence: true
-  validates :content, length: { minimum: 1}
+  validates :content, length: { minimum: 1,message: 'Review cannot be blank' }
 end
