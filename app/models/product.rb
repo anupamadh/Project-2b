67685967a71ba664b90a7d4c8edeab4df0a7c100
Anupama Dhir <sabhssabhs@yahoo.com>
2016-08-06 08:37:22 +0800

    added case insensitive search

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   has_many :reviews, dependent: :destroy
 def self.search(search)
-  where("name LIKE ?", "%#{search}%")
+  where("lower(name) LIKE ?", "%#{search}%")
 end
 end
